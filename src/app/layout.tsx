3726f0312adb47f52fce789c52b39d1f16a9353a
zim89 <121241841+zim89@users.noreply.github.com>
2025-04-01 17:48:38 +0300

    Initial commit

---
+++ src/app/layout.tsx
@@ -0,0 +1,25 @@
+import type { Metadata } from 'next'
+import { Nunito } from 'next/font/google'
+import './globals.css'
+
+const nunito = Nunito({
+  variable: '--font-nunito',
+  subsets: ['latin', 'cyrillic'],
+  weight: ['400', '500', '600', '700', '800', '900'],
+})
+
+export const metadata: Metadata = {
+  title: 'PizzaRush',
+}
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode
+}>) {
+  return (
+    <html lang='en'>
+      <body className={`${nunito.variable} antialiased`}>{children}</body>
+    </html>
+  )
+}
---
+++ src/app/page.tsx
@@ -0,0 +1,10 @@
+import { Button } from '@/shared/components/ui'
+
+export default function Home() {
+  return (
+    <div>
+      <h2>Home page</h2>
+      <Button variant='outline'>ecceec</Button>
+    </div>
+  )
+}
